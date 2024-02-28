90ac72a4252933d2a06c70c742a6d3dcbafa1cf3
Gabriele <gabriele.palma.bool@gmail.com>
2024-02-28 12:20:53 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,67 @@
-<script >
+<script>
 //importo libreria axios
-import axios from 'axios'
+import axios from "axios";
 
 //importo componenti figli
-import Myheader from './components/Myheader.vue'
-import Mymain from './components/Mymain.vue'
-import Myfooter from './components/Myfooter.vue'
+import Myheader from "./components/Myheader.vue";
+import Mymain from "./components/Mymain.vue";
+import Myfooter from "./components/Myfooter.vue";
 
 //importo store
-import { store } from './store'
-
+import { store } from "./store";
 
 export default {
+  components: {
+    Myheader,
+    Mymain,
+    Myfooter,
+  },
 
-    components: {
-        Myheader,
-        Mymain,
-        Myfooter
+  data() {
+    return {
+      store,
+    };
+  },
+
+  methods: {
+    getFilms() {
+      let myURL = store.apiURL;
+
+      let call = false;
+
+      //Se è stato selezionato qualcosa dal selettore
+      if (store.searchText !== "") {
+        (myURL += `&${store.query}=${store.searchText}`), (call = true);
+      }
+
+      console.log(myURL);
+      //prima chiamata axios
+      if (call === true) {
+        axios
+          .get(myURL)
+          .then((res) => {
+            store.FilmsList = res.data.results;
+            console.log(store.FilmsList);
+          })
+          .catch((err) => {
+            console.log("Errori", err);
+          });
+      }
     },
+  },
 
-    data() {
-        return {
-            store,
-        }
-
-    },
-
-    methods: {
-        getFilms() {
-
-            let myURL = store.apiURL
-
-            let call = false
-
-            //Se è stato selezionato qualcosa dal selettore
-            if (store.searchText !== "") {
-
-                myURL += `&${store.query}=${store.searchText}`,
-
-                    call = true
-
-            }
-
-            console.log(myURL);
-            //prima chiamata axios
-            if (call === true) {
-
-                axios.get(myURL).then((res => { store.FilmsList = res.data.results; console.log(store.FilmsList); })).catch((err) => {
-                    console.log("Errori", err);
-                });
-            }
-
-
-        }
-    },
-
-    created() {
-        this.getFilms();
-    }
-
-}
+  created() {
+    this.getFilms();
+  },
+};
 </script>
 
 <template>
-    <Myheader @serchfilm="getFilms" />
-    <Mymain />
-    <Myfooter />
+  <Myheader @serchfilm="getFilms" />
+  <Mymain />
+  <Myfooter />
 </template>
 
 <style lang="scss">
-@use './styles/general.scss';
+@use "./styles/general.scss";
 </style>
